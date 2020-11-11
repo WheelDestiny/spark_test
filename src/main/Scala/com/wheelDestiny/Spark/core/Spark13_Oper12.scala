d94dca4d7c469417e9b5f3fed6a2cb3c38567c43
wheelDestiny <672845190@qq.com>
2020-11-12 00:54:14 +0800

    add file

---
+++ src/main/Scala/com/wheelDestiny/Spark/core/Spark13_Oper12.scala
@@ -0,0 +1,42 @@
+package com.wheelDestiny.Spark.core
+
+import org.apache.spark.{Partitioner, SparkConf, SparkContext}
+
+object Spark13_Oper12 {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    //创建Spark上下文对象
+    val sc = new SparkContext(conf)
+
+    //map算子
+
+    val listRDD = sc.makeRDD(Array((1,"aaa"),(2,"bbb"),(3,"ccc"),(4,"ddd")),4)
+//    重分区
+//    println(listRDD.partitionBy(new org.apache.spark.HashPartitioner(2)).collect().mkString(","))
+
+    listRDD.partitionBy(new MyPartitioner(2)).glom().collect().foreach(i =>{
+      i.foreach(print)
+      println()
+    })
+
+  }
+
+  class MyPartitioner(partitioner : Int) extends Partitioner{
+    override def numPartitions: Int = {
+      partitioner
+    }
+
+    override def getPartition(key: Any): Int = key match {
+      case null => 0
+      case _ => nonNegativeMod(key.hashCode, numPartitions)
+    }
+
+    def nonNegativeMod(x: Int, mod: Int): Int = {
+      val rawMod = x % mod
+      rawMod + (if (rawMod < 0) mod else 0)
+    }
+  }
+
+}
